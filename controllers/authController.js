76aaccd307ed7143b69ac49a76124b19a1232b3a
Tusharshrivastav <ts3657@gmail.com>
2021-11-15 07:15:57 +0530

    added singup api hasing password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,36 @@
-exports.singupController = (req, res) => {
-  res.send("its working");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//singup controller
+exports.singupController = async (req, res) => {
+  //destructuring from req.body
+  const { username, email, password } = req.body;
+  try {
+    const user = await User.find({ email });
+    //checking user already exists
+    if (user) {
+      return res.status(400).json({ errorMessage: "Email already exists " });
+    }
+    // creating new user
+    const newUser = new User();
+    newUser.username = username;
+    newUser.email = email;
+
+    //hashing user password while signup
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(password, salt);
+
+    //saveing new user
+    await newUser.save();
+
+    res.json({
+      successMessage: "Registration success. Please signin.",
+    });
+  } catch (error) {
+    console.log("signupController error: ", err);
+    res.status(500).json({
+      errorMessage: "Server error",
+    });
+  }
 };
