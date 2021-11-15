a22b365c2eab7357256861c3494370048a171965
Tusharshrivastav <ts3657@gmail.com>
2021-11-15 07:00:09 +0530

    added user model

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -17,13 +17,11 @@ exports.signinValidator = [
 exports.validatorResult = (req, res, next) => {
   const result = validationResult(req);
   const hasErrors = !result.isEmpty();
-
   if (hasErrors) {
     const firstError = result.array()[0].msg;
     return res.status(400).json({
       errorMessage: firstError,
     });
   }
-
   next();
 };
